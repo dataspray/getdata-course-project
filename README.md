b10d5252f718a9429e2ce696b30e2dc3774151a1
dataspray <beansbeforebreakfast@gmail.com>
2015-04-25 23:33:17 -0500

    1.0

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# getdata-course-project
+# Getting and Cleaning Data Course Project
+
+The R script contained in this repo, run_analysis.R, will use data captured from the accelerometer and gyroscope of smartphones from 30 volunteers to create a "tidy dataset" to be used for further processing.
+
+The dataset used, is described in detail here:  
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+
+This dataset must reside in the same folder as the R script whereto the working directory should be set. The data may be downloaded here:
+
+https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
+The script, run_analysis.R, will:
+
+1) Merges the training and the test sets to create one data set.
+2) Extracts only the measurements on the mean and standard deviation for each measurement. 
+3) Uses descriptive activity names to name the activities in the data set
+4) Appropriately labels the data set with descriptive variable names. 
+5) From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+The script proceeds as follows:
+
+First the "Subject", "Activity", and "Accelerometer and Gyroscope" observations from both the training and test datasets are loaded them merged. 
+The descriptive activity names are loaded and joined with the activity observations.
+The names of the variables for the "Accelerometer and Gyroscope" observations are loaded and altered to make "tidy" then applied to the merged data.
+Finally the script makes use of "chained"commands to
+* Join columns from all data sets arriving at our "one data set"
+* Select only the measurements on the mean and standard deviation required for the tidy dataset
+* Average of each variable grouping by activity and subject
+* Write out the tidy dataset called "TidyData.txt"
+
--- run_analysis.R
+++ run_analysis.R
@@ -6,20 +6,19 @@
 
 library(dplyr)
 
-tblSubject <- bind_rows(read.table("UCI HAR Dataset/train/subject_train.txt",col.names="SubjectId"),
+tblSubject <- bind_rows(read.table("UCI HAR Dataset/train/subject_train.txt",col.names="SubjectId"),    # For the "Subject" observations, load and merge training and test data sets.
                         read.table("UCI HAR Dataset/test/subject_test.txt",col.names="SubjectId"))
 
-tblActivity <- bind_rows(read.table("UCI HAR Dataset/train/y_train.txt",col.names="ActivityId"),
+tblActivity <- bind_rows(read.table("UCI HAR Dataset/train/y_train.txt",col.names="ActivityId"),        # For the "Activity" observations, load and merge training and test data sets.
                          read.table("UCI HAR Dataset/test/y_test.txt",col.names="ActivityId")) %>%
-               inner_join(read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("ActivityId","Activity")), by = "ActivityId")
+               inner_join(read.table("UCI HAR Dataset/activity_labels.txt",col.names=c("ActivityId","Activity")), by = "ActivityId")  # Load then join the activity descriptions
   
 dataColNames <- make.names(select(read.table("UCI HAR Dataset/features.txt"), V2)[[1]], unique = TRUE)  # all the names (column V2) in features.txt as a vector
 dataColNames <- gsub("_+$", "", gsub("\\.+", "_", dataColNames))                                        # strings of 1 or more periods become 1 underscore ... but none at the end
-tblData <- bind_rows(tbl_df(read.table("UCI HAR Dataset/train/X_train.txt",col.names=dataColNames)),
+tblData <- bind_rows(tbl_df(read.table("UCI HAR Dataset/train/X_train.txt",col.names=dataColNames)),    # For the "Accelerometer and Gyroscope" observations, load and merge training and test data sets.
                      tbl_df(read.table("UCI HAR Dataset/test/X_test.txt",col.names=dataColNames)))
 
-tbl <- bind_cols(tblSubject, tblActivity, tblData) %>%                                  # Join columns from all data sets arriving at our "one data set"
-       select(SubjectId, Activity, matches("mean|std", ignore.case = TRUE)) %>%
-       group_by(SubjectId, Activity) %>% summarise_each(funs(mean))
-
-write.table(tbl, "TidyData.txt", row.name=FALSE)
+bind_cols(tblSubject, tblActivity, tblData) %>%                                                         # Join columns from all data sets arriving at our "one data set"
+    select(SubjectId, Activity, matches("mean|std", ignore.case = TRUE)) %>%                            # Only the measurements on the mean and standard deviation required for the tidy dataset
+    group_by(SubjectId, Activity) %>% summarise_each(funs(mean)) %>%                                    # Average of each variable for each activity and each subject
+    write.table("TidyData.txt", row.name=FALSE)                                                         # Create a tidy dataset called TidyData.txt
